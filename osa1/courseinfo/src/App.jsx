7b2e907f37f47f2f638497ab8a968388b703cdf5
SuviAnnina <suvi.varjoranta@gmail.com>
2024-08-09 13:47:19 +0300

    add exercise 1.5

--- osa1/courseinfo/src/App.jsx
+++ osa1/courseinfo/src/App.jsx
@@ -1,7 +1,7 @@
 const Header = (props) => {
   return (
     <>
-      <h1>{props.name}</h1>
+      <h1>{props.name.name}</h1>
     </>
   )
 }
@@ -36,27 +36,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header name={course} />
-      <Content parts={parts} />
-      <Total exerciseAmount={parts} />
+      <Content parts={course.parts} />
+      <Total exerciseAmount={course.parts} />
     </div>
   )
 }
