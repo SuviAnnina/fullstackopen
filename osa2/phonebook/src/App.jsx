ab79d43cdbb8db536c7d50bd2a3176a762c2a148
SuviAnnina <suvi.varjoranta@gmail.com>
2024-08-11 20:45:30 +0300

    Add exercise 2.6 phonebook

---
+++ osa2/phonebook/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    {
+      name: 'Arto Hellas',
+      id: "1"
+    }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault()
+    const personObject = {
+      name: newName,
+      id: String(persons.length + 1),
+    }
+    setPersons(persons.concat(personObject))
+    setNewName("")
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addPerson}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+        {persons.map((person) =>
+          <p key={person.id}>{person.name}</p>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default App
