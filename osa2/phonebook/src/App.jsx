22015ca1a8615599cadbd65c6d6bd9ef7ba19c69
SuviAnnina <suvi.varjoranta@gmail.com>
2024-08-11 21:29:14 +0300

    Add exercise 2.8

--- osa2/phonebook/src/App.jsx
+++ osa2/phonebook/src/App.jsx
@@ -4,13 +4,19 @@ const App = () => {
   const [persons, setPersons] = useState([
     {
       name: 'Arto Hellas',
-      id: "1"
+      id: '1',
+      number: '0401234567'
     }
   ])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
 
   const addPerson = (event) => {
@@ -22,9 +28,11 @@ const App = () => {
       const personObject = {
         name: newName,
         id: String(persons.length + 1),
+        number: newNumber
       }
       setPersons(persons.concat(personObject))
-      setNewName("")
+      setNewName('')
+      setNewNumber('')
     }
   }
 
@@ -42,13 +50,20 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <div>
         {persons.map((person) =>
-          <p key={person.id}>{person.name}</p>
+          <p key={person.id}>{person.name} {person.number}</p>
         )}
       </div>
     </div>
