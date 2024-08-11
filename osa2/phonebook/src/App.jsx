fc6a2f1f782cfa8c4fad5e06a1b2d0bc29dba2c8
SuviAnnina <suvi.varjoranta@gmail.com>
2024-08-11 22:33:39 +0300

    Add exercise 2.9

--- osa2/phonebook/src/App.jsx
+++ osa2/phonebook/src/App.jsx
@@ -6,10 +6,27 @@ const App = () => {
       name: 'Arto Hellas',
       id: '1',
       number: '0401234567'
-    }
+    },
+    {
+      name: 'Ada Lovelace',
+      number: '39-44-5323523',
+      id: '2'
+    },
+    {
+      name: 'Mary Poppendick',
+      number: '39-23-6423122',
+      id: '3'
+    },
+    {
+      name: 'Dan Abramov',
+      number: '12-43-234345',
+      id: '4'
+    },
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [search, setSearch] = useState('')
+  const [isFiltered, setIsFiltered] = useState(false)
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -17,6 +34,16 @@ const App = () => {
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchChange = (event) => {
+    if (event.target.value.length > 0) {
+      setSearch(event.target.value)
+      setIsFiltered(true)
+    } else if (event.target.value.length === 0) {
+      setSearch('')
+      setIsFiltered(false)
+    }
   }
 
   const addPerson = (event) => {
@@ -36,11 +63,20 @@ const App = () => {
     }
   }
 
-  const nameExists = (name) => persons.map((person) => person.name).includes(name)
+  const nameExists = (name) => persons.map((person) => person.name.toLowerCase()).includes(name.toLowerCase())
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with
+        <input
+          value={search}
+          onChange={handleSearchChange}
+        />
+      </div>
+
+      <h2>Add new</h2>
       <form onSubmit={addPerson}>
         <div>
           name:
@@ -62,10 +98,14 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <div>
-        {persons.map((person) =>
-          <p key={person.id}>{person.name} {person.number}</p>
-        )}
+        {isFiltered
+          ? <div>{persons.filter((person) => person.name.toLowerCase().includes(search.toLowerCase())).map((person) => <p key={person.id}>{person.name} {person.number}</p>)} </div>
+          : <div>{persons.map((person) =>
+            <p key={person.id}>{person.name} {person.number}</p>
+          )}
+          </div>}
       </div>
+
     </div>
   )
 }
